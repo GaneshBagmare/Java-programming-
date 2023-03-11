f56ca9f5bd37a2afe0cb24e828f84ff2a82dc798
GaneshBagmare <125943036+GaneshBagmare@users.noreply.github.com>
2023-03-11 09:24:56 +0530

    Add files via upload

---
+++ Task4.java
@@ -0,0 +1,13 @@
+/*******/
+import java.util.Scanner;
+class Task4{
+	public static void main(String[] args)
+	{
+		int i=1;
+		do 
+		{
+			System.out.println("Ganesh B");
+			i++;
+		}while(i<=1000);
+	}
+}
