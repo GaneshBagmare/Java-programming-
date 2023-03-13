520a2525ee646d74abd4bce884416926eac3c7b9
GaneshBagmare <125943036+GaneshBagmare@users.noreply.github.com>
2023-03-13 09:59:58 +0530

    Add files via upload

---
+++ Pattern3.java
@@ -0,0 +1,16 @@
+/**************/
+
+class Pattern3
+{
+	public static void main(String[] args)
+	{
+		for (int i=4;i>=1 ;--i ) 
+		{
+			for (int j=1;j<=i ;++j )
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
