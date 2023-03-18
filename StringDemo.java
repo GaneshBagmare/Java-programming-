930e5c5dda4c37396937057d35978da20fec8ada
GaneshBagmare <125943036+GaneshBagmare@users.noreply.github.com>
2023-03-18 10:26:13 +0530

    Add files via upload

---
+++ StringDemo.java
@@ -0,0 +1,8 @@
+class StringDemo{
+	public static void main(String[] args) {
+		String str="I Love Java";
+		System.out.println(str);
+		str=str+2;
+		System.out.println(str);
+	}
+}
---
+++ StringDemo8.java
@@ -0,0 +1,11 @@
+class StringDemo8{
+	public static void main(String[] args) {
+		String s1="Ganesh";
+		boolean b=s1.equals("Ganesh");
+		System.out.println(b);
+		System.out.println(s1.equals("Ganesh"));
+		String s2="Gokul";
+		boolean b1=s1.equals(s2);
+		System.out.println(b1);
+	}
+}
